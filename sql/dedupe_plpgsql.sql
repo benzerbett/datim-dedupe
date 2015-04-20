13769bc5c920c20f1ecf33e207a1025c30d97de4
Jason P. Pickering <jason.p.pickering@gmail.com>
2015-04-20 11:13:11 +0200

    Added new paramater 'ty' which will accept PURE or CROSSWALK to return different duplicate types.

--- sql/dedupe_plpgsql.sql
+++ sql/dedupe_plpgsql.sql
@@ -2,7 +2,7 @@ DROP FUNCTION  IF EXISTS view_duplicates(character,character varying,
 boolean,integer,integer,character varying );
 
 CREATE OR REPLACE FUNCTION view_duplicates(ou character (11),pe character varying(15),rs boolean default false,
-ps integer default 50,pg integer default 1,dt character varying(50) default 'ALL' ) 
+ps integer default 50,pg integer default 1,dt character varying(50) default 'ALL',ty character varying(50) default 'PURE' ) 
 RETURNS setof duplicate_records AS  $$
  DECLARE
  returnrec duplicate_records;
@@ -27,6 +27,7 @@ RETURNS setof duplicate_records AS  $$
  lastupdated timestamp without time zone
  ) ON COMMIT DROP;
  
+IF ty = 'PURE'::character varying(50) THEN
  
  EXECUTE 'INSERT INTO temp1
  SELECT DISTINCT
@@ -59,11 +60,11 @@ RETURNS setof duplicate_records AS  $$
  ''i29foJcLY9Y'',''xxo1G5V1JG2'', ''STL4izfLznL'') ) ) 
  AND dv1.periodid = (SELECT DISTINCT periodid from _periodstructure
  where iso = ''' || $2 || ''' LIMIT 1)
-ORDER BY dv1.dataelementid,dv1.categoryoptioncomboid,dv1.attributeoptioncomboid,dv1.sourceid';
+ AND dv1.value ~ (''^(-?0|-?[1-9][0-9]*)(\.[0-9]+)?(E[0-9]+)?$'')';
 
   
 /*Group ID. This will be used to group duplicates. */
-ALTER TABLE temp1 ADD COLUMN group_id character(32);
+ALTER TABLE temp1 ADD COLUMN group_id text;
 UPDATE temp1 SET group_id = dataelementid::text ||  categoryoptioncomboid::text || sourceid::text  ;
 CREATE INDEX idx_group_ids ON temp1 (group_id);
 
@@ -76,9 +77,10 @@ DELETE FROM temp1 where attributeoptioncomboid !=
  (SELECT categoryoptioncomboid from _categoryoptioncomboname where categoryoptioncomboname ~('^\(00000'))
 AND value = '0';
 
-
+/*Get rid of any groups which remain with less than two members*/
 DELETE FROM temp1 where group_id IN (SELECT group_id from temp1   WHERE attributeoptioncomboid != (SELECT categoryoptioncomboid
 FROM _categoryoptioncomboname where categoryoptioncomboname ~('00000 De-duplication adjustment'))  GROUP BY group_id HAVING COUNT(*) < 2);
+
 /*Duplication status*/
  
  ALTER TABLE temp1 ADD COLUMN duplication_status character varying(50) DEFAULT 'UNRESOLVED';
@@ -102,6 +104,87 @@ WHERE a.dedupe_time <= b.data_time)
 AND group_id IN (SELECT DISTINCT group_id from temp1 where value ~('^[-|0]') and attributeoptioncomboid = (SELECT categoryoptioncomboid
   FROM _categoryoptioncomboname where categoryoptioncomboname ~('00000 De-duplication adjustment'))); 
  
+END IF;
+/*End PURE Dedupe logic*/
+
+IF ty = 'CROSSWALK'::character varying(50) THEN
+
+EXECUTE 'INSERT INTO temp1 
+ SELECT DISTINCT
+ ta.sourceid,
+ ta.periodid,
+ ta.dataelementid,
+ ta.categoryoptioncomboid,
+ ta.attributeoptioncomboid,
+ trim(ta.value) ,''CROSSWALK''::character varying(20) as duplicate_type,
+ ta.lastupdated
+ from datavalue ta
+ INNER JOIN
+ (SELECT DISTINCT
+ dv1.sourceid,
+ dv1.periodid,
+ dv1.dataelementid,
+ map.ta_dataelementid,
+ dv1.categoryoptioncomboid,
+ dv1.attributeoptioncomboid
+ from datavalue dv1
+ INNER JOIN  (SELECT * FROM _view_dsd_ta_crosswalk ) map
+ on dv1.dataelementid = map.dsd_dataelementid
+ WHERE dv1.value ~ (''^(-?0|-?[1-9][0-9]*)(\.[0-9]+)?(E[0-9]+)?$'')
+ ) dsd
+ on ta.sourceid = dsd.sourceid
+ AND ta.periodid = dsd.periodid
+ and ta.dataelementid = dsd.ta_dataelementid
+ and ta.categoryoptioncomboid = dsd.categoryoptioncomboid
+ INNER JOIN _orgunitstructure ous on ta.sourceid = ous.organisationunitid
+ and ous.uidlevel3 = ''' || $1 || '''
+  AND ta.periodid = (SELECT DISTINCT periodid from _periodstructure
+ where iso = ''' || $2 || ''')';
+
+
+
+/*Group ID. This will be used to group duplicates. */
+ALTER TABLE temp1 ADD COLUMN group_id text;
+UPDATE temp1 SET group_id = dataelementid::text ||  categoryoptioncomboid::text || sourceid::text  ;
+CREATE INDEX idx_group_ids ON temp1 (group_id);
+/*Exclude any zero dupe components*/
+DELETE FROM temp1 where attributeoptioncomboid NOT IN
+ (SELECT categoryoptioncomboid from _categoryoptioncomboname where categoryoptioncomboname ~('^\(0000[0|1]'))
+AND value = '0';
+
+/*DELETE FROM temp1 where group_id IN (SELECT * from temp1   WHERE attributeoptioncomboid != (SELECT categoryoptioncomboid
+FROM _categoryoptioncomboname where categoryoptioncomboname ~('00001 De-duplication adjustment'))  GROUP BY group_id HAVING COUNT(*) < 2);*/
+
+
+
+/*Duplication status*/
+ 
+ ALTER TABLE temp1 ADD COLUMN duplication_status character varying(50) DEFAULT 'UNRESOLVED';
+/*Only resolve non-legacy dedupes*/
+ UPDATE temp1 set duplication_status = 'RESOLVED'
+ where group_id IN (SELECT DISTINCT group_id FROM temp1
+ WHERE attributeoptioncomboid = (SELECT categoryoptioncomboid
+  FROM _categoryoptioncomboname where categoryoptioncomboname ~*('00001 De-duplication adjustment')))
+AND group_id NOT IN (SELECT a.group_id FROM (
+SELECT group_id,MAX(lastupdated) as dedupe_time from temp1 
+WHERE attributeoptioncomboid = (SELECT categoryoptioncomboid
+  FROM _categoryoptioncomboname where categoryoptioncomboname ~*('00001 De-duplication adjustment'))
+GROUP BY group_id ) a
+INNER JOIN (
+SELECT group_id,MAX(lastupdated) as data_time from temp1 
+WHERE attributeoptioncomboid != (SELECT categoryoptioncomboid
+FROM _categoryoptioncomboname where categoryoptioncomboname ~*('00001 De-duplication adjustment'))
+GROUP BY group_id ) b
+on a.group_id = b.group_id
+WHERE a.dedupe_time <= b.data_time); 
+ 
+
+END IF;
+/*End CROSSWALK Dedupe logic*/
+
+
+
+
  IF rs = FALSE THEN 
  DELETE FROM temp1 where duplication_status ='RESOLVED';
  END IF;
