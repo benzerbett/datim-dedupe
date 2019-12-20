0ed5cb474a5ecb545514f0a89cbd6edbbaf79bfe
Jakub Flaska <jflaska@baosystems.com>
2019-12-20 14:32:12 -0500

    cache working!

--- devserver.js
+++ devserver.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const app = express();
+const requestProxy = require("express-request-proxy");
 
-app.use(express.static('.'));
+app.use(express.static('./dev'));
+
+const credentials = process.env.DHIS_USERNAME + ":" + process.env.DHIS_PASSWORD;
+
+// app.all("/api/*", requestProxy({
+//     url: process.env.DHIS_BASEURL + '/api/user/me',
+//     headers: {
+//         Authorization: "Basic " + new Buffer(credentials).toString("base64")
+//     }
+// }));
+
+app.all("/api/*", (req,res,next)=>{
+    let f = requestProxy({
+        url: process.env.DHIS_BASEURL + '/api/me',
+        headers: {
+            Authorization: "Basic " + new Buffer(credentials).toString("base64")
+        }
+    });
+    f(req,res,next);
+});
+
+app.all('/test', requestProxy({url:'http://www.google.com/'}));
 
 app.listen(8000);
