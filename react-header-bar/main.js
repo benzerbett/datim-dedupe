bb73e394b854a4839ec994a25d956b6e31ec5236
Jakub <jflaska@baosystems.com>
2020-01-24 14:50:35 -0500

    text header bar

--- react-header-bar/main.js
+++ react-header-bar/main.js
@@ -1,1 +1,10 @@
-console.log('react world');
+class Main extends React.Component {
+    render() {
+        return React.createElement('div', null, `header bar`);
+    }
+}
+
+ReactDOM.render(
+    React.createElement(Main, {toWhat: 'World'}, null),
+    document.getElementById('header-bar')
+);
