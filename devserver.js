42a09f0ff80dd431c9d5b915dbc93e7bd59b4007
Jakub <jflaska@baosystems.com>
2020-01-24 11:14:15 -0500

    repeairing dev server

--- devserver.js
+++ devserver.js
@@ -2,6 +2,8 @@ const express = require('express'),
  app = express(),
  requestProxy = require("express-request-proxy");
 
+if (!process.env.DHIS_BASEURL || !process.env.DHIS_USERNAME || !process.env.DHIS_PASSWORD) throw new Error ('please set process.env.DHIS* variables')
+
 const credentials = process.env.DHIS_USERNAME + ":" + process.env.DHIS_PASSWORD;
 
 app.use(express.static('./dev'));
